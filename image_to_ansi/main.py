f7fc88397ce36f2a5b50ca02e7344221515294eb
Alumy94 <alexeyka.xx@gmail.com>
2020-11-28 20:58:07 +0700

    Add methods

--- image_to_ansi/main.py
+++ image_to_ansi/main.py
@@ -1,13 +1,19 @@
+from cv2 import cv2
+
+
 class Colors(object):
     """Container of all required colors."""
     black = 30
+    black_bgr = (0, 0, 0)
     red = 31
+    red_bgr = (0, 0, 255)
     green = 32
     yellow = 33
     blue = 34
     magenta = 35
     cyan = 36
     white = 37
+    white_bgr = (255, 255, 255)
     reset = 0
 
 
@@ -15,9 +21,9 @@ def generate_color(color, bright=False):
     """Use color code to create the correct construction to use this color"""
     if color != 0:
         if not bright:
-            return "\u001b[{}m\u001b[{}m".format(color, color+10)
+            return "\u001b[{}m\u001b[{}m".format(color, color + 10)
         else:
-            return "\u001b[{};1m\u001b[{};1m".format(color, color+10)
+            return "\u001b[{};1m\u001b[{};1m".format(color, color + 10)
     else:
         return "\u001b[{}m".format(color)
 
@@ -60,19 +66,47 @@ def save_to_file(text, file_name, override=True):
         print("File not found: \"{}\"".format(file_name))
 
 
-def convert_image(image, text_size=(80, 25), bg=True):
-    out_str = " "
-    if bg:
-        for x in range(text_size[1]):
-            for j in range(text_size[0]):
-                out_str += generate_draw(Colors.white, bright=True)
-            out_str += "\n"
+def get_average_section_color(image, x, y, width, height):
+    # Check if is it possible to get color
+    if x >= image.shape[0] or y >= image.shape[1]:
+        return None
 
-    return out_str
+    # Initialize variables
+    iteration = 0
+    ac_b, ac_g, ac_r = (0, 0, 0)
+
+    # Main loop which goes over the image's section
+    for dx in range(width):
+        for dy in range(height):
+            # Check if is it possible to get color
+            if x + dx >= image.shape[0] or y + dy >= image.shape[1]:
+                return None
+
+            # Calculate new average color
+            iteration += 1
+            n_b, n_g, n_r = image[x + dx, y + dy]
+            ac_b = (ac_b + n_b)
+            ac_g = (ac_g + n_g)
+            ac_r = (ac_r + n_r)
+
+    ac_b /= iteration
+    ac_g /= iteration
+    ac_r /= iteration
+
+    # Return colors
+    return int(ac_b), int(ac_g), int(ac_r)
+
+
+def load_image(image_path):
+    """Return loaded image"""
+    try:
+        image = cv2.imread(image_path)
+        return image
+    except FileNotFoundError:
+        print("File not found")
 
 
 def print_test_image():
-    save_to_file(convert_image("img"), "image_to_ansi/output/test.ans")
     print_ans_file("image_to_ansi/output/test.ans")
 
 
