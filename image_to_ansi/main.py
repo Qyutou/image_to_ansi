216bdab981d3c5ad18701c9083b802a4f114b9b9
Alumy94 <alexeyka.xx@gmail.com>
2020-11-29 21:33:51 +0700

    Change the method of usage this application

--- image_to_ansi/main.py
+++ image_to_ansi/main.py
@@ -1,101 +1,40 @@
-from cv2 import cv2
-import numpy as np
-from image_to_ansi.colors import Colors
+from colors import Colors
+import image_handler as ih
+import click
+import sys
+import re
 
 
-def load_image(image_path):
-    """Return loaded image"""
-    try:
-        image = cv2.imread(image_path)
-        return image
-    except FileNotFoundError:
-        print("File not found")
-
-
-def scale_image(image, new_size=[80, 25]):
-    """Scale the image to certain size"""
-    # If the image is square
-    if image.shape[0] == image.shape[1]:
-        if new_size[0] <= new_size[1]:
-            new_size[1] = int(new_size[0] / 2)
-        else:
-            new_size[0] = int(new_size[1] * 2)
-
-    # Calculate coefficients
-    coefficient_x = image.shape[0] / new_size[0]
-    coefficient_y = image.shape[1] / new_size[1]
-    new_image = np.zeros((new_size[1], new_size[0], 3), np.uint8)
-
-    # Main loop which scale image
-    for y in range(new_size[1]):
-        for x in range(new_size[0]):
-            average_color = get_average_section_color(image,
-                                                      int(y * coefficient_y), int(x * coefficient_x),
-                                                      int(coefficient_y), int(coefficient_x))
-            if average_color is not None:
-                new_image[y, x] = average_color
-
-    return new_image
-
-
-def get_average_section_color(image, x, y, width, height):
-    """Get average color in section"""
-    # Check if is it possible to get color
-    if x >= image.shape[0] or y >= image.shape[1]:
-        return None
-
-    # Initialize variables
-    iteration = 0
-    ac_b, ac_g, ac_r = (0, 0, 0)
-
-    # Main loop which goes over the image's section
-    for dx in range(width):
-        for dy in range(height):
-            # Check if is it possible to get color
-            if not x + dx >= image.shape[0] or y + dy >= image.shape[1]:
-                # Calculate new average color
-                iteration += 1
-                n_b, n_g, n_r = image[x + dx, y + dy]
-                ac_b = (ac_b + n_b)
-                ac_g = (ac_g + n_g)
-                ac_r = (ac_r + n_r)
-
-    ac_b /= iteration
-    ac_g /= iteration
-    ac_r /= iteration
-
-    # Return colors
-    return int(ac_b), int(ac_g), int(ac_r)
-
-
-def convert_image_to_text(image, text_size=[80, 25], show_image=False):
+def convert_image_to_text(image, text_size=[80, 25], background=True, character=None):
+    """This method simply use the algorithm which translate image to text."""
     # Load the colors
-    colors = Colors("image_to_ansi/resources/colors.json")
+    colors = Colors("resources/colors.json")
 
     # Scale image
-    scaled_image = scale_image(image, new_size=text_size)
+    print("Scaling Image...")
+    scaled_image = ih.scale_image(image, new_size=text_size)
 
     # Get the text version of image
-    output_text = get_text_by_image(scaled_image, colors)
+    print("Creating text Version...")
+    output_text = get_text_by_image(scaled_image, colors, background=background, character=character)
 
-    # save text
-    save_to_file(output_text, "image_to_ansi/output/test.ans")
-
-    # Draw text
-    print_ans_file("image_to_ansi/output/test.ans")
-
-    if show_image:
-        cv2.imshow("image", scaled_image)
-        cv2.waitKey()
+    return output_text
 
 
-def get_text_by_image(image, colors):
-    output_text = " "
+def get_text_by_image(image, colors, background=True, character=None):
+    """Get text from image."""
+    output_text = ""
     for x in range(image.shape[0]):
         for y in range(image.shape[1]):
+            # Get the color of current pixel
             image_color = image[x, y]
+            # Find the closest color from the possible ansi-colors
             color = colors.get_closest_color(image_color)
-            output_text += colors.generate_draw(color)
+            # Add new character
+            if character is None:
+                output_text += colors.generate_draw(color, background=background)
+            else:
+                output_text += colors.generate_colored_text(character, color, background=background)
         output_text += "\n"
     return output_text
 
@@ -128,11 +67,77 @@ def save_to_file(text, file_name, override=True):
         print("File not found: \"{}\"".format(file_name))
 
 
+@click.group()
+@click.version_option("1.0.0")
 def main():
     """A simple application which can convert image to .ans file"""
-    convert_image_to_text(load_image("image_to_ansi/resources/image.png"))
+    pass
+
+
+def get_size(size):
+    """
+    Get the size from WWWxHHH format
+    It also check if the format is correct
+    """
+    if re.match(r"^\d+x\d+$", size) is not None:
+        sizes = [int(value) for value in re.findall(r"\d+", size)]
+        return sizes
+    else:
+        print("Size should be in WWWxHHH format")
+        return 80, 25
+
+
+@main.command()
+@click.option("--s", "--size", "size",
+              default="80x25", show_default=True, type=str,
+              help="Size of the text.")
+@click.option("--bg", "--background", "background",
+              default=True, show_default=True, type=bool,
+              help="Should the characters use colored bg or not.")
+@click.option("--c", "--character", "character",
+              default=None, show_default=False, type=str,
+              help="Character which used to draw image. (Default: pseudographics)")
+@click.argument("input_path")
+@click.argument("output_path")
+def convert(size, background, character, input_path, output_path):
+    """Convert image to text, and save it to file."""
+    # Get converted image
+    text = convert_image_to_text(ih.load_image(input_path),
+                                 text_size=get_size(size), background=background, character=character)
+
+    # Save text
+    save_to_file(text, output_path)
+
+    # Draw text
+    print_ans_file(output_path)
+    pass
+
+
+@main.command()
+@click.option("--s", "--size", "size",
+              default="80x25", show_default=True, type=str,
+              help="Size of the text.")
+@click.option("--bg", "--background", "background",
+              default=True, show_default=True, type=bool,
+              help="Should the characters use colored bg or not.")
+@click.option("--c", "--character", "character",
+              default=None, show_default=False, type=str,
+              help="Character which used to draw image. (Default: pseudographics)")
+@click.argument("path")
+def draw(size, background, character, path):
+    """Draw image(.png or .jpg) or ansi file(.ans)."""
+    # Check if file extension is image or ansi file
+    if path.endswith(".ans"):
+        # Print the .ans file
+        print_ans_file(path)
+    elif path.endswith(".png") or path.endswith(".jpg"):
+        # Print the converted version
+        print(convert_image_to_text(ih.load_image(path),
+                                    text_size=get_size(size), background=background, character=character))
     pass
 
 
 if __name__ == "__main__":
+    args = sys.argv
     main()
+    pass
