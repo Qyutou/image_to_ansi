8c5242bf1cfb0e3560e66d01730fd33cbede2d05
Qyutou <qyutou@gmail.com>
2020-11-30 11:23:14 +0700

    Add vertical and horizontal images support

--- image_to_ansi/image_handler.py
+++ image_to_ansi/image_handler.py
@@ -11,8 +11,9 @@ def load_image(image_path):
         print("File not found")
 
 
+"""
 def get_average_section_color(image, x, y, width, height):
-    """Get average color in section"""
+    \"\"\"Get average color in section\"\"\"
     # Check if is it possible to get color
     if x >= image.shape[0] or y >= image.shape[1]:
         return None
@@ -41,32 +42,52 @@ def get_average_section_color(image, x, y, width, height):
 
     # Return colors
     return int(ac_b), int(ac_g), int(ac_r), int(ac_a)
+"""
 
 
-def scale_image(image, new_size=(80, 25)):
+def scale_image(image, new_size=(100, 40)):
     """Scale the image to certain size"""
     required_size = [new_size[0], new_size[1]]
-    # If the image is square
-    if image.shape[0] == image.shape[1]:
-        if required_size[0] <= required_size[1]:
-            required_size[1] = int(required_size[0] / 2)
-        else:
-            required_size[0] = int(required_size[1] * 2)
 
-    # Calculate coefficients
-    coefficient_x = image.shape[0] / required_size[0]
-    coefficient_y = image.shape[1] / required_size[1]
-    new_image = np.zeros((required_size[1], required_size[0], 4), np.uint8)
+    # Calculate resized image size
+    required_size = calculate_resized_size((image.shape[1], image.shape[0]), required_size)
 
-    # Main loop which scale image
-    for y in range(required_size[1]):
-        for x in range(required_size[0]):
-            average_color = get_average_section_color(image,
-                                                      int(y * coefficient_y), int(x * coefficient_x),
-                                                      int(coefficient_y), int(coefficient_x))
-            if average_color is not None:
-                new_image[y, x] = average_color
+    # resize image
+    new_image = cv2.resize(image, (required_size[0] * 2, required_size[1]), interpolation=cv2.INTER_AREA)
 
     return new_image
 
+
+def calculate_resized_size(current_size, required_size):
+    result_size = required_size
+
+    # If the image is square
+    if current_size[0] == current_size[1]:
+        if required_size[0] <= required_size[1]:
+            result_size[1] = int(required_size[0])
+        else:
+            result_size[0] = int(required_size[1])
+
+    # If height is smaller than width
+    if current_size[1] < current_size[0]:
+        # If height is smaller than width
+        if required_size[1] < required_size[0]:
+            result_size[1] = int(required_size[1])
+            result_size[0] = int((current_size[0] / (current_size[1] / result_size[1])))
+
+        else:
+            result_size[0] = int(required_size[0])
+            result_size[1] = int((current_size[1] / (current_size[0] / result_size[0])))
+
+    # If width is smaller than height
+    if current_size[0] < current_size[1]:
+        # If height is smaller than width
+        if required_size[1] < required_size[0]:
+            result_size[0] = int(required_size[0])
+            result_size[1] = int((current_size[1] / (current_size[0] / result_size[0])))
+        else:
+            result_size[1] = int(required_size[1])
+            result_size[0] = int((current_size[0] / (current_size[1] / result_size[1])))
+
+    return result_size
 
